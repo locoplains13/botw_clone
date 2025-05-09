d74eb727f63ae6dd2c86137623b81b69497cacff
Angel Roberto Andrade <alroc1234@hotmail.com>
2025-05-09 00:02:36 -0700

    added second attack 'slice' as a combo after chop

--- Scenes/entities/Player/godette_skin.gd
+++ Scenes/entities/Player/godette_skin.gd
@@ -1,12 +1,20 @@
 extends Node3D
 
 @onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
+@onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
+
+var attacking := false
 
 func set_move_state(state_name: String) -> void:
 	move_state_machine.travel(state_name)
 
 func attack() -> void:
-	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	if not attacking:
+		attack_state_machine.travel('Slice' if $SecondAttackTimer.time_left else 'Chop')
+		$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func attack_toggle(value: bool):
+	attacking = value
 
 func defend(forward: bool) -> void:
 	var tween = create_tween()
